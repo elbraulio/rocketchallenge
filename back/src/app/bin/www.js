5d8db33cbf6e79e0c662436f4ce6984ffe76994c
elbraulio <elbraulio274@gmail.com>
2020-01-14 11:00:33 -0300

    remove ups files

--- back/src/app/bin/www.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var http_1 = __importDefault(require("http"));
-var index_1 = __importDefault(require("../index"));
-index_1.default.set('port', 8000);
-http_1.default.createServer(index_1.default).listen(8000);
